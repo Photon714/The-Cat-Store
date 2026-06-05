a298c16ed73a1e41066eefae7c18cf9f169dbd1f
Jiraiya Sama <nameshshende@gmail.com>
2026-06-06 00:07:36 +0530

    made a cat controller which will give all the cats from the db

--- Backend/Controller/cat-controller.js
+++ Backend/Controller/cat-controller.js
@@ -1,10 +1,9 @@
 const asyncHandler = require("express-async-handler"); // use it to avoid typing try and catch every time
 const Cat = require("../models/catModel.js");
-const data = require("../Data/data.js");
-//@desc Get all items
-//@route GET/api/UI
-//@access private
 
+//@desc Get all cats
+//@route GET/api/cat
+//@access public
 const getItems = asyncHandler(async (req, res) => {
   const cats = await Cat.find();
   res.status(200).json(cats);
