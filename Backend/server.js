dbb2975341153306776710c90c7946046004f3c5
Jiraiya Sama <nameshshende@gmail.com>
2026-06-05 23:57:35 +0530

    added changed routs and comments

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use("/api/UI", require("./Route/route.js")); //middleware ie ye url pe call aaya to ye behjo
+app.use("/api/cat", require("./Route/cat-route.js")); //middleware ie ye url pe call aaya to ye behjo
+app.use("/api/cart", require("./Route/cart-route.js")); //middleware ie ye url pe call aaya to ye behjo
 app.use("/api/user", require("./Route/user.js")); //this is for authentication of users
 app.use(errorHandler);
 app.listen(port, () => {
