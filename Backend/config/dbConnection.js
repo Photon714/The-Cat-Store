3634a7f25625d318d7c89aad536362d23ec0b51b
Jiraiya Sama <nameshshende@gmail.com>
2026-06-05 23:58:24 +0530

    shifted the connection in try catch block and added comments

--- Backend/config/dbConnection.js
+++ Backend/config/dbConnection.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
-
+/*
+Using the connect constant to connect to the database using the connection string which was given by mongodb(web)
+*/
 const connectDb = async () => {
-  const connect = await mongoose.connect(process.env.CONNECTION_STRING);
   try {
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
     console.log(
       "Database Connected successfully",
       connect.connection.host,
