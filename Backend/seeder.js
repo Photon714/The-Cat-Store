f01d6e1b8ef9c0b808ad9bc8cfebc5d2a30fb791
Jiraiya Sama <nameshshende@gmail.com>
2026-06-05 23:56:58 +0530

    added comments used to send live data to backend

--- Backend/seeder.js
+++ Backend/seeder.js
@@ -1,3 +1,7 @@
+/* 
+Used to send our data ie cats to backend such that backend se cats fetch honge and if we add new cat same will happen in backend
+each page refresh pe pura cats load hoga ig
+*/
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const Cat = require("./models/catModel.js");
@@ -6,13 +10,13 @@ const data = require("./Data/data.js");
 const importToMongoose = async () => {
   try {
     await mongoose.connect(process.env.CONNECTION_STRING);
-    console.log("connecting to mongodb");
+    // connecting to mongodb
 
     await Cat.deleteMany();
-    console.log("deleting previous old version data");
+    // deleting previous old version data
 
     await Cat.insertMany(data);
-    console.log("inserting new cats:>");
+    //inserting new cats:>
     process.exit();
   } catch (error) {
     console.error("Failed to import data:", error);
