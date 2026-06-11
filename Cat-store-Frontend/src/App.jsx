9154fbb912787fc83c26db360234e409202b93a6
Jiraiya Sama <nameshshende@gmail.com>
2026-06-12 00:24:47 +0530

    added showallcats

---
+++ Cat-store-Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ShowAllCats from "./components/catGallery.jsx";
+
+function App() {
+  return (
+    <div>
+      <ShowAllCats />
+    </div>
+  );
+}
+export default App;
