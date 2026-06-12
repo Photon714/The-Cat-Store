8fb22c2be20c8aba21f49c622fadf5ac6af74c13
Jiraiya Sama <nameshshende@gmail.com>
2026-06-12 20:55:02 +0530

    Used the routes

--- Cat-store-Frontend/src/App.jsx
+++ Cat-store-Frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ShowAllCats from "./components/catGallery.jsx";
+import LogIn from "./components/Login.jsx";
+import SignUp from "./components/SignUp.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <ShowAllCats />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LogIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/catGallery" element={<ShowAllCats />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
