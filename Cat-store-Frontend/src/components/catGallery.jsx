1b58bc9a323b581ce4359f7a2898cc1e2ebb170c
Jiraiya Sama <nameshshende@gmail.com>
2026-06-12 23:06:48 +0530

    Added add to cart button jispe on click it send the catid and quantity to backend and made a quantity ka input and also if token doesnt exists then using navigate to throw em into loggin page

--- Cat-store-Frontend/src/components/catGallery.jsx
+++ Cat-store-Frontend/src/components/catGallery.jsx
@@ -3,10 +3,14 @@ import React, { useState, useEffect } from "react";
 import Card from "./Card.jsx";
 import "../css/catGallery.css";
 import Header from "./header.jsx";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function ShowAllCats() {
   const [cats, setCats] = useState([]);
-
+  const [cartMessage, setCartMessage] = useState("");
+  const navigate = useNavigate();
+  const [quantities, setQuantities] = useState({});
   useEffect(() => {
     const getCats = async () => {
       const fetchedCats = await fetchCats();
@@ -21,19 +25,71 @@ function ShowAllCats() {
     getCats();
   }, []);
 
+  const handleQuantityChange = (catId, val) => {
+    setQuantities({
+      ...quantities,
+      [catId]: val,
+    });
+  };
+
+  const handleAddToCart = async (catId) => {
+    const token = localStorage.getItem("accessToken");
+    const quan = Number(quantities[catId]) || 1;
+    if (!token) {
+      alert("loggin to access cart and add items to it");
+      navigate("/");
+      return;
+    }
+    try {
+      const response = await axios.post(
+        "http://localhost:5001/api/cart/add",
+        { catId: catId, quantity: quan },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      console.log("Cart updated:", response.data);
+      setCartMessage("Added to cart successfully! 🐾");
+      setTimeout(() => setCartMessage(""), 3000);
+    } catch (error) {
+      console.log("Cart Error:", error.response?.data || error.message);
+      setCartMessage("Failed to add to cart.");
+      setTimeout(() => setCartMessage(""), 3000);
+    }
+  };
+
   return (
     <>
       <Header />
       <div className="cardContainer">
         {cats.map((data) => (
-          <Card
-            key={data._id}
-            name={data.name}
-            age={data.age}
-            breed={data.breed}
-            cost={data.price}
-            description={data.description}
-          />
+          <div key={data._id} className="cardWrapper">
+            <Card
+              key={data._id}
+              name={data.name}
+              age={data.age}
+              breed={data.breed}
+              cost={data.price}
+              description={data.description}
+            />
+            <div className="cardActionRow">
+              <button
+                className="galleryAddToCartBtn"
+                onClick={() => handleAddToCart(data._id)}
+              >
+                Add to Cart
+              </button>
+              <input
+                type="number"
+                min="1"
+                className="quantityInput"
+                value={quantities[data._id] ?? 1} // Value comes from our state object
+                onChange={(e) => handleQuantityChange(data._id, e.target.value)}
+              />
+            </div>
+          </div>
         ))}
       </div>
     </>
